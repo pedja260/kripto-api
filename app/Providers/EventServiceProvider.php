879d87c0963232572d171d169a15e02fefebea32
pedja260 <pedja260@gmail.com>
2021-10-18 13:24:42 +0200

    proba

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,6 +15,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
+        'App\Events\PriceAlertEvent' => [
+            'App\Listeners\PriceAlertEventListener',
+        ],
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
--- routes/channels.php
+++ routes/channels.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('alertChannel', function ($price_alert) {
+    return $price_alert;
+});
